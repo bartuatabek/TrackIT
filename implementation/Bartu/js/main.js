d6f251c49e1c21e192d0e581f3264ffbbb0e71fa
Bartu Atabek <bartu.atabek@icloud.com>
2019-05-21 01:13:26 +0300

    Kanban Complete

--- implementation/Bartu/js/main.js
+++ implementation/Bartu/js/main.js
@@ -1,5 +1,5 @@
 //set body overflow hidden;
-$("body").css("overflow", "hidden")
+$(".Workflow-container").css("overflow-y", "hidden")
 
 var Workflow_config = {};
 Workflow_config.height = ($(document).height() - 30);
@@ -24,88 +24,65 @@ $(function() {
     }).disableSelection();
 });
 
+var binder = function() {
+	//for drag and drop of the workspaces
+	$(function() {
+			$(".ul-parent").sortable({
+					handle: '.handle',
+	        items: '> :not(#archive)',
+					placeholder: "highlight",
+					start: function(event, ui) {
+							ui.placeholder.height(ui.helper.outerHeight());
+							ui.placeholder.width(ui.helper.outerWidth());
+							console.log(ui, "ui")
+					}
+			}).disableSelection();
+	});
+
+	//for shuffling of the cards
+	$(function() {
+			$("li #cards").sortable({
+					connectWith: "li #cards",
+					placeholder: "highlight",
+					cancel: ".cancel_drag",
+//					items: '> :not(.cancel_drag)',
+					start: function(event, ui) {
+							ui.placeholder.height(ui.helper.outerHeight());
+							ui.placeholder.width(ui.helper.outerWidth());
+							console.log(ui, "ui")
+					}
+			}).disableSelection();
+	});
+
+	(function($) {
+			//bind delete workspace
+			$('.delete-workspace').off('click').on("click", function() {
+					var list_id = $(this).attr('id');
+					$(this).parent().parent().remove();
+					$.post("request.php", {func_name: "remove_lists", list_id: list_id})
+					.done(function(data) {
+					});
+			});
+	})($);
+}
+
 //basic initializing format
 function init(workflowName) {
-
-    var binder = function() {
-
-        //for drag and drop of the workspaces
-        $(function() {
-            $(".ul-parent").sortable({
-                handle: '.handle',
-                //items: '> :not(.create_workspace)',
-                placeholder: "highlight",
-                start: function(event, ui) {
-                    ui.placeholder.height(ui.helper.outerHeight());
-                    ui.placeholder.width(ui.helper.outerWidth());
-                    console.log(ui, "ui")
-                }
-            }).disableSelection();
-        });
-
-        //for shuffling of the cards
-        $(function() {
-            $("li #cards").sortable({
-                connectWith: "li #cards",
-                placeholder: "highlight",
-                cancel: ".cancel_drag",
-                items: '> :not(.cancel_drag)',
-                start: function(event, ui) {
-                    ui.placeholder.height(ui.helper.outerHeight());
-                    ui.placeholder.width(ui.helper.outerWidth());
-                    console.log(ui, "ui")
-                }
-            }).disableSelection();
-        });
-
-        (function($) {
-            //bind modal window to add a card;
-            $('.add-a-card').on("click", function() {
-                //set modal Config
-                $('.modalBox').modalBox({
-                    width: 600,
-                    height: 500,
-                    top: 100,
-                    iconImg: 'Close.png',
-                    iconClose: false
-
-                });
-                $('.modalBox').modalBox('open');
-                $('.modalBox').html("please add card")
-            })
-        })($);
-
-        (function($) {
-            //bind delete workspace
-            $('.delete-workspace').off('click').on("click", function() {
-                $(this).parent().parent().remove();
-            });
-
-        })($);
-    }
-
     if (workflowName != '') {
-
         //for adding a workflow
-        basic_workflow = '<li class="li-parent no_padding_bottom"><div class = "handle"><div class="name-of-workflow">' + workflowName + '</div><div class="delete-workspace"><span class="glyphicon glyphicon-trash">x</span></div></div>';
+        basic_workflow = '<li class="li-parent no_padding_bottom"><div class = "handle"><div class="name-of-workflow">' + workflowName + '</div><div class="delete-workspace" id="' + list_id + '"><span class="glyphicon glyphicon-trash">x</span></div></div>';
         basic_workflow += '<ul id="cards">';
-        basic_workflow += '<li><i class="fas fa-align-left"></i>Card Name</li><li><i class="fas fa-align-left"></i>Card Name</li><li><i class="fas fa-align-left"></i>Card Name</li><li><i class="fas fa-align-left"></i>Card Name</li><li><i class="fas fa-align-left"></i>Card Name</li><li class="cancel_drag"><div class="add-a-card">Add a card</div></li>';
+        basic_workflow += '<li class="cancel_drag" id="hidden" value="0"></li><li class="cancel_drag"><div class="add-a-card">Add a card</div></li>';
         basic_workflow += '</ul></li>';
-
+			
         //set input value to null
         $('#add_workflow').val('');
-
         binder();
         workflowName = '';
     } else {
         binder();
     }
-
-
-
-} //!init()
-
-
+}
 
 $('#add_workflow').bind('keydown', function(e) {
     var code = e.keyCode || e.which;
@@ -113,65 +90,138 @@ $('#add_workflow').bind('keydown', function(e) {
         if (this.value === '') {
             init();
         } else {
+						var list_title = this.value;
             init(this.value);
             $(basic_workflow).insertBefore('#for_prepend_purpose');
             init();
-
+						$.post("request.php", {func_name: "add_lists", title: list_title})
         }
 
         $('.Workflow-container').animate({
             scrollLeft: 100000
         });
-
     }
-
-
 });
 
+$(document).ready(function() {
+	fetchBoard();
+});
 
+function addCard(list_id) {
+    $('#createCardModal').modal();
+    onClickStr = "add_card('" + list_id + "')";
+    document.getElementById('cadd').setAttribute( "onClick", onClickStr);	
+}
 
+function add_card(list_id) {
+    let name = document.getElementById("cname").value;
+    let description = document.getElementById("cdescription").value;
+			
+    $.post( "request.php", { func_name: "add_card", title: name, description: description, list_id : list_id})
+        .done(function( data ) {
+        $('#createCardModal').modal('hide');
+        document.getElementById("cname").value = "";
+        document.getElementById("cdescription").value = "";
+				
+				location.reload(true);
+    });
+}
 
-function dumpAllElements() {
-    //	var number_of_workflows = $('.li-parent').length -1;
-    var output = [];
-    var output_object = {};
+function showCard(card_id) {
+    $.post( "request.php", { func_name: "show_card", card_id: card_id})
+        .done(function( data ) {
+        let dataparsed = JSON.parse(data);
+        document.getElementById("cpastetitle").innerHTML = dataparsed[0].title;
+        document.getElementById("cpastedescription").innerHTML += dataparsed[0].description;
+        onClickStr = "assign_user('" + card_id + "')";
+    		document.getElementById('assuserbut').setAttribute( "onClick", onClickStr );
+        
+				onClickStr = "archive_card('" + card_id + "')";
+				document.getElementById('archivebut').setAttribute( "onClick", onClickStr );
+        
+        onClickStr = "delete_card('" + card_id + "')";
+        document.getElementById('cdelete').setAttribute( "onClick", onClickStr );
+        fetch_assignedusers(card_id);
+        $('#showCardModal').modal();
+    });
+}
 
+function fetch_assignedusers(card_id) {
+    $.post( "request.php", { func_name: "fetch_assignedusers", card_id:card_id})
+        .done(function( data ) {
+        document.getElementById("pparticipants").innerHTML="";
 
-    var total_cards = $('.li-parent ul li').length;
-    var all_workspace_cards_array = [];
-    var single_workspace_cards_array = [];
-    for (var i = 0; i < total_cards; i++) {
-        var current_card = $($('.li-parent ul li')[i]);
+        let dataparsed = JSON.parse(data);
 
-        if (current_card.attr('class') === 'cancel_drag') {
-            all_workspace_cards_array.push(single_workspace_cards_array);
-            single_workspace_cards_array = [];
-        } else {
-            single_workspace_cards_array.push(current_card.html());
-        }
+        dataparsed.forEach(function(element) {
+            document.getElementById("pparticipants").innerHTML+= 
+                '<a style="font-size:13px;">'+ element.name +'<span style="color: darkgray"> #' + element.user_id +'</span> </a>'+
+                '<a style="font-size:13px;">|</a>';
+        });
 
-    }
+    });
+}
 
+function assign_user(card_id) {
+    var user_id = document.getElementById("assUserText").value;
+    $.post( "request.php", { func_name: "assign_user", card_id:card_id, user_id:user_id})
+        .done(function( data ) {
+        
+    });
+}
 
-    var number_of_workflows = $('.li-parent .handle .name-of-workflow').length;
-    //	console.log(number_of_workflows)
-    for (var i = 0; i < number_of_workflows; i++) {
+function archive_card(card_id) {
+	$.post("request.php", {func_name: "is_archived", card_id: card_id})
+		.done(function(data) {
+			console.log("b");
+			location.reload(true);
+		});
+}
 
-        output_object.workflow_name = $($('.li-parent .handle .name-of-workflow')[i]).html();
-        output_object.cards = all_workspace_cards_array[i];
+function delete_card(card_id) {
+    $.post( "request.php", { func_name: "delete_card", card_id:card_id})
+        .done(function( data ) {
+        document.getElementById("assUserText").value = "";
+        $('#showCardModal').modal('hide');
+				location.reload(true);
+    });
+}
 
-        output.push(output_object);
-
-        output_object = {};
-
-
-    }
-
-    for (var i = 0; i < number_of_workflows; i++) {
-        output_object = $($('.li-parent .handle .name-of-workflow')[i]).html();
-
-
-    }
-
-    return output;
+function fetchBoard() {
+	// check archive
+	$.post("request.php", {func_name: "create_archive"})
+		.done(function(data) {
+		});
+		
+	// get lists
+	$.post("request.php", {func_name: "fetch_lists"})
+		.done(function(lists) {
+			let dataparsed = JSON.parse(lists);
+			dataparsed.forEach(function(element) {
+				  var list_id = element.list_id;
+					var list_name = element.title;
+					var workflow;
+					if (list_name == "Archive") {
+						workflow = '<li class="li-parent no_padding_bottom" id="archive"><div class="handle"><div class="name-of-workflow">' + list_name + '</div></div><ul id="cards" class="ui-sortable"><li class="cancel_drag" id="hidden" value="' + list_id + '"></li>';
+					} else {
+						workflow = '<li class="li-parent no_padding_bottom"><div class="handle"><div class="name-of-workflow">' + list_name + '</div><div class="delete-workspace" id="' + list_id + '"><span class="glyphicon glyphicon-trash">x</span></div></div><ul id="cards" class="ui-sortable"><li class="cancel_drag" id="hidden" value="' + list_id + '"></li>';
+					}
+					
+					// fetch cards
+					$.post("request.php", {func_name: "fetch_cards", list_id: list_id})
+					.done(function(cards) {
+						let dataparsed = JSON.parse(cards);
+						dataparsed.forEach(function(element) {
+							var card_id = element.card_id;
+							var card_title = element.title;
+							
+							workflow += '<li onclick="showCard(' + card_id + ')"><i class="fas fa-align-left"></i>' + card_title + '</li>';
+						});
+						workflow += '<li onclick="addCard(' + list_id + ')" class="cancel_drag"><div class="add-a-card">Add a card</div></li>';
+						workflow +='</ul></li>';
+						binder();
+						$(workflow).insertBefore('#for_prepend_purpose');
+					});
+			});
+		});
 }
--- implementation/Bartu/request.php
+++ implementation/Bartu/request.php
@@ -4,8 +4,9 @@ session_start();
 
 $_SESSION['project_id'] = 1; //this is mock id
 $_SESSION['user_id'] = 1; //this is mock id
+$_SESSION['board_id'] = 1; //this is mock id
 $project_id = $_SESSION['project_id'];
-$func_name = mysqli_real_escape_string($connection, $_POST['func_name'] );
+$func_name = mysqli_real_escape_string($connection, $_POST['func_name']);
 
 switch($func_name) {
     case 'remove_privilage':
@@ -13,6 +14,48 @@ switch($func_name) {
         break;
 		case 'add_privilage':
         add_privilage($connection, $_POST['user_id']);
+        break;
+		case 'fetch_lists':
+        fetch_lists($connection, $_SESSION['board_id']);
+        break;
+		case 'fetch_cards':
+        fetch_cards($connection, $_POST['list_id'], $_SESSION['user_id']);
+        break;
+		case 'add_lists':
+        add_lists($connection, $_POST['title'], $_SESSION['board_id']);
+        break;
+		case 'remove_lists':
+        remove_lists($connection, $_POST['list_id'], $_SESSION['board_id']);
+        break;
+		case 'create_archive':
+				create_archive($connection);
+				break;
+		case 'is_archived':
+				is_archived($connection, $_SESSION['user_id'], $project_id, $_POST['card_id']);
+				break;
+		case 'archive_card':
+        archive_card($connection, $_SESSION['user_id'], $project_id, $_POST['card_id']);
+        break;
+		case 'unarchive_card':
+        unarchive_card($connection, $_SESSION['user_id'], $project_id, $_POST['card_id']);
+        break;
+		case 'change_card_list':
+        change_card_list($connection, $_POST['list_id'], $_POST['card_id'], $_SESSION['user_id']);
+        break;
+		case 'add_card':
+        add_card($connection, $_POST['title'], $_POST['description'],$_SESSION['user_id'],$_POST['list_id']);
+        break;
+    case 'show_card':
+        show_card($connection, $_POST['card_id']);
+        break;
+		case 'fetch_assignedusers':
+        fetch_assignedusers($connection, $_POST['card_id']);
+        break;
+    case 'assign_user':
+        assign_user($connection, $_POST['card_id'],$_POST['user_id']);
+        break;
+    case 'delete_card':
+        delete_card($connection, $_POST['card_id']);
         break;
     default:
         echo "-1";
@@ -30,5 +73,156 @@ function add_privilage($connection, $user_id) {
 		$insert_query = "INSERT INTO PrivilegedUser (user_id) VALUES ('$user_id')";
 		mysqli_query($connection, $del_query);
 		mysqli_query($connection, $insert_query);
+}
+
+function fetch_lists($connection, $board_id) {
+	$dbdata = array();
+	$sql = "SELECT list_id, title FROM Lists WHERE board_id='$board_id'";
+	$result = mysqli_query($connection, $sql);
+	
+	while($row=mysqli_fetch_assoc($result)) {
+        $dbdata[]=$row;
+    }
+    echo json_encode($dbdata);
+}
+
+function fetch_cards($connection, $list_id, $user_id) {
+	$dbdata = array();
+	$sql = "SELECT card_id, title FROM Cards WHERE list_id='$list_id' AND user_id='$user_id'";
+	$result = mysqli_query($connection, $sql);
+	
+	while($row=mysqli_fetch_assoc($result)) {
+        $dbdata[]=$row;
+    }
+    echo json_encode($dbdata);
+}
+
+function add_lists($connection, $title, $board_id) {
+	$insert_query = "INSERT INTO Lists (title, board_id) VALUES ('$title', '$board_id')";
+	mysqli_query($connection, $insert_query);
+}
+
+function remove_lists($connection, $list_id, $board_id) {
+	$del_query = "DELETE FROM Lists WHERE list_id='$list_id' AND board_id='$board_id'";  
+	mysqli_query($connection, $del_query);
+}
+
+function create_archive($connection) {
+	$sql = "SELECT list_id FROM Lists";
+	$result = mysqli_query($connection, $sql);
+	
+	if($result-> num_rows == 0) {
+		$title = 'Archive';
+		$board_id = $_SESSION['board_id'];
+		add_lists($connection, $title, $board_id);
+	}
+}
+
+function is_archived($connection, $user_id, $project_id, $card_id) {
+	$sql = "SELECT archive_id FROM Archive NATURAL JOIN Actions WHERE user_id='$user_id' AND project_id='$project_id' AND card_id='$card_id'";
+	$result = mysqli_query($connection, $sql);
+	
+	if($result-> num_rows == 0) {
+		archive_card($connection, $user_id, $project_id, $card_id);
+	}else{
+		unarchive_card($connection, $user_id, $project_id, $card_id);
+	}
+}
+
+function archive_card($connection, $user_id, $project_id, $card_id) {
+	$insert_actions = "INSERT INTO Actions (user_id, project_id, card_id) VALUES ('$user_id', '$project_id', '$card_id')";
+	mysqli_query($connection, $insert_actions);
+	
+	$sql = "SELECT item_id FROM Actions WHERE user_id='$user_id' AND project_id='$project_id' AND card_id='$card_id'";
+	$result = mysqli_query($connection, $sql);
+	$value = mysqli_fetch_assoc($result);
+	$item_id =$value['item_id'];
+	$insert_archive = "INSERT INTO Archive (archived_date, item_id) VALUES (CURDATE(), '$item_id')";
+	mysqli_query($connection, $insert_archive);
+	change_card_list($connection, 1, $card_id, $user_id);
+}
+
+function unarchive_card($connection, $user_id, $project_id, $card_id) {
+	$sql = "SELECT archive_id FROM Archive NATURAL JOIN Actions WHERE user_id='$user_id' AND project_id='$project_id' AND card_id='$card_id'";
+	$result = mysqli_query($connection, $sql);
+	$value = mysqli_fetch_assoc($result);
+	$archive_id =$value['archive_id'];
+	
+	$delete_actions = "DELETE FROM Actions WHERE user_id='$user_id' AND project_id='$project_id' AND card_id='$card_id'";
+	mysqli_query($connection, $delete_actions);
+	change_card_list($connection, $archive_id, $card_id, $user_id);
+}
+
+function change_card_list($connection, $list_id, $card_id, $user_id) {
+	$sql = "UPDATE Cards SET list_id='$list_id' WHERE card_id='$card_id' AND user_id='$user_id'";
+	mysqli_query($connection, $sql);
+}
+
+function add_card($connection, $title, $description,$user_id,$list_id) {
+        $sql=mysqli_query
+        ($connection,
+         "
+            INSERT INTO Cards (title, description, status,start_date,user_id,list_id) 
+            VALUES ('$title', '$description', 'new',CURDATE(),$user_id,$list_id);
+		"
+        );
+}
+
+function show_card($connection,$card_id) {
+    $dbdata = array();
+
+    $sql=mysqli_query
+        ($connection,
+         "
+			SELECT 	description, title
+			FROM 	Cards
+			WHERE 	card_id	=$card_id;
+		"
+        );
+
+    while($row=mysqli_fetch_assoc($sql))
+    {
+        $dbdata[]=$row;
+    }
+    echo json_encode($dbdata);
+}
+
+function fetch_assignedusers($connection,$card_id) {
+    $dbdata = array();
+
+    $sql=mysqli_query
+        ($connection,
+         "
+			SELECT 	name, user_id
+			FROM 	AssignsCard NATURAL JOIN User
+			WHERE 	card_id	=$card_id;
+		"
+        );
+
+    while($row=mysqli_fetch_assoc($sql))
+    {
+        $dbdata[]=$row;
+    }
+    echo json_encode($dbdata);
+}
+
+function assign_user($connection,$card_id,$user_id) {
+    $sql=mysqli_query
+        ($connection,
+         "
+            INSERT INTO AssignsCard (card_id, user_id) 
+            VALUES ($card_id,$user_id);
+		"
+        );
+}
+
+function delete_card($connection,$card_id) {
+    $sql=mysqli_query
+        ($connection,
+         "
+            DELETE FROM Cards
+			WHERE card_id='$card_id';
+		"
+        );
 }
 ?>
